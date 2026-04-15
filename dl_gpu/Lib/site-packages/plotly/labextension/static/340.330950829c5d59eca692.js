dac8c4fa0873e491340ad285470fd96c1de32239
Relll06HUB <darrell.traveller@gmail.com>
2026-04-15 13:11:12 +0700

    Remove oversized files

--- dl_gpu/Lib/site-packages/plotly/labextension/static/340.330950829c5d59eca692.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:8fe791079c4f38571f03dfc164a219cc1d5cfc00dfb82bdb2a7629471701d929
-size 4833499
--- dl_gpu/Lib/site-packages/plotly/labextension/static/remoteEntry.bbf25df5110252151416.js
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:bbf25df5110252151416bc6e40b47caf98d674c6945459de8557e06c5b4ff2ba
-size 3808
